3c310225f241e68d6138e51c5a9fc0e778ecdf6e
Aya Mohamed <ayamohamed3420@gmail.com>
2023-06-09 20:57:21 +0300

    added setting maps and payment

---
+++ YRStor/Modules/Currency/View/RadioButton/RadioButtonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RadioButtonTableViewCell.swift
+//  YRStor
+//
+//  Created by Aya Mohamed Ahmed on 08/06/2023.
+//
+
+import UIKit
+
+class RadioButtonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
